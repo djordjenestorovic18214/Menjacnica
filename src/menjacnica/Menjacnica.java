4aa3f7e26b168d6e955a8fc2a86cb6f8e9abe15d
djordjenestorovic18214 <djordje_95@hotmail.com>
2016-03-19 23:01:36 +0100

    Napisana metoda koja dodaje kurs.

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -7,17 +7,13 @@ import interfejs.MenjacnicaInterfejs;
 public class Menjacnica implements MenjacnicaInterfejs {
 
 	public void dodajKurs(Valuta valuta, Kurs kurs) {
-		// TODO Auto-generated method stub
-
+		valuta.getKursevi().add(0, kurs);
 	}
 
 	public void obrisiKurs(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
 	}
 
 	public Kurs pronadjiKurs(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
