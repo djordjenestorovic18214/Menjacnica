1a469d9dea24d1931ea45824bbb568d4369e8a5d
djordjenestorovic18214 <djordje_95@hotmail.com>
2016-03-19 22:00:55 +0100

    Napravljena klasa Menjacnica koja implementira interfejs MenjacnicaInterfejs. Metode jos nisu funkcionalne.

---
+++ src/menjacnica/Menjacnica.java
@@ -0,0 +1,24 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import interfejs.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	public void dodajKurs(Valuta valuta, Kurs kurs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void obrisiKurs(Valuta valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Kurs pronadjiKurs(Valuta valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
