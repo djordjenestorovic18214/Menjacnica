d501f76b05ac6a02441059d92f81699537ae37a1
djordjenestorovic18214 <djordje_95@hotmail.com>
2016-03-19 22:55:10 +0100

    Klasa Menjacnica implementirana do kraja na novoj grani implementacijainterfejsa.

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -1,23 +1,37 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
-
 import interfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
 
 	public void dodajKurs(Valuta valuta, Kurs kurs) {
-		// TODO Auto-generated method stub
-
+		valuta.getKursevi().add(0, kurs);
 	}
 
 	public void obrisiKurs(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		if(valuta.getKursevi() == null) {
+			System.out.println("Lista sa kursevima je prazna!");
+		return;
+		}
+		for (int i = 0; i < valuta.getKursevi().size(); i++) {
+			if(valuta.getKursevi().get(i).getDatum() == datum) {
+				valuta.getKursevi().remove(valuta.getKursevi().get(i));
+				break;
+			}
+		}
 	}
 
 	public Kurs pronadjiKurs(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		if(valuta.getKursevi() == null) {
+			System.out.println("Lista sa kursevima je prazna!");
+		return null;
+		}
+		for (int i = 0; i < valuta.getKursevi().size(); i++) {
+			if(valuta.getKursevi().get(i).getDatum() == datum) {
+				return valuta.getKursevi().get(i);
+			}
+		}
 		return null;
 	}
 
